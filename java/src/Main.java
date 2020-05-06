579d41cacdd936563f91458b472ac0dbae8535a5
nafnaz <C16715071@mytudublin.ie>
2020-05-06 10:38:02 +0100

    Main class

---
+++ java/src/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author nafy
+ */
+import processing.core.PApplet;
+
+public class Main
+{
+
+    public void startUI()
+    {
+        String[] a = {"MAIN"};
+        System.out.print(a[0]);
+
+    }
+    public static void main(String[] args)
+    {
+        Main main = new Main();
+        main.startUI();
+    }
+}
